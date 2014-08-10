eba415587758365dc2c3ccd9fd1074be3cd7ef58
mgushee <matt@gushee.net>
2014-08-10 10:43:38 -0600

    Created message encoding/decoding test cases.

--- apps/osc_data/lib/osc_data.ex
+++ apps/osc_data/lib/osc_data.ex
@@ -1,2 +1,53 @@
 defmodule OSC.Data do
+  defprotocol Encoding do
+    @doc "Encodes OSC messages and their arguments."
+    def encode(data)
+  end
+
+  defmodule Message do
+    defstruct address: "", type_tags: ",", args: []
+  end
+
+  defmodule Bundle do
+    defstruct time_tag: 0.0, elements: []
+  end
+
+  defmodule Blob do
+    defstruct data: ""
+  end
+
+  defimpl Encoding, for: Message do
+    def encode(%Message{address: ""}) do
+      {:error, "Address is missing."}
+    end
+    def encode(%Message{address: addr, type_tags: tt, args: args}) do
+      {tags, body} = Enum.reduce args, {tt, ""}, (
+        fn (arg, {acc_tags, acc_data}) ->
+          {tag, data} = encode arg
+          {acc_tags <> tag, acc_data <> data}
+        )
+      (pad_string addr) <> (pad_string tags) <> body
+    end
+  end
+
+  defimpl Encoding, for Bundle do
+  end
+
+  defimpl Encoding, for Integer do
+    def encode(n) when n > 2147483647 or n < -2147483648
+      {:error, "Number out of range."}
+    end
+    def encode(n) do
+      <<n :: 32>>
+    end
+  end
+
+  defimpl Encoding, for Float do
+  end
+
+  defimpl Encoding, for String do
+  end
+
+  defimpl Encoding, for Blob do
+  end
 end
--- apps/osc_data/test/osc_data_test.exs
+++ apps/osc_data/test/osc_data_test.exs
@@ -1,13 +1,40 @@
 defmodule OSC.DataTest do
   use ExUnit.Case
 
-  defp message1 do
+  setup do
+    assert String.byte_size message1_bin == 32
+    assert String.byte_size message2_bin == 40
+  end
+
+  defp message1_data do
+    {:message, "/oscillator/4/frequency", [440.0]}
+  end
+  defp message1_bin do
     "/oscillator/4/frequency" <> <<0>> <> ",f" <> <<0, 0>> <> <<0x43, 0xdc, 0, 0>>
   end
 
-  defp message2 do
+  defp message2_data do
+    {:message, "/foo", [1000, -1, "hello", 1.234, 5.678]}
+  end
+  defp message2_bin do
     "/foo" <> <<0, 0, 0, 0>> <> ",iisff" <> <<0, 0>> <> <<0x3e8 :: 32>> <>
       <<0xffffffff :: 32>> <> "hello" <> <<0 :: 24>> <> <<0x3f, 0x9d, 0xf3, 0xb6>> <>
       <<0x40, 0xb5, 0xb2, 0x2d>>
+  end
+
+  test "Encode message1" do
+    assert OSC.Data.encode message1_data == message1_bin
+  end
+
+  test "Encode message2" do
+    assert OSC.Data.encode message2_data == message2_bin
+  end
+
+  test "Decode message1" do
+    assert OSC.Data.decode message1_bin == message1_data
+  end
+
+  test "Decode message2" do
+    assert OSC.Data.decode message2_bin == message2_data
   end
 end
