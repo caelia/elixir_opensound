ee24020e878b4bc81ae892d7b5f45385b011607a
mgushee <matt@gushee.net>
2014-08-09 19:21:55 -0600

    Created reference strings for testing.

--- apps/osc_data/test/osc_data_test.exs
+++ apps/osc_data/test/osc_data_test.exs
@@ -1,7 +1,13 @@
 defmodule OSC.DataTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  defp message1 do
+    "/oscillator/4/frequency" <> <<0>> <> ",f" <> <<0, 0>> <> <<0x43, 0xdc, 0, 0>>
+  end
+
+  defp message2 do
+    "/foo" <> <<0, 0, 0, 0>> <> ",iisff" <> <<0, 0>> <> <<0x3e8 :: 32>> <>
+      <<0xffffffff :: 32>> <> "hello" <> <<0 :: 24>> <> <<0x3f, 0x9d, 0xf3, 0xb6>> <>
+      <<0x40, 0xb5, 0xb2, 0x2d>>
   end
 end
