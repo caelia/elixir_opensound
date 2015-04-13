80fbe4d438acb6fd452e9fbc99afcf7023ac697c
mgushee <matt@gushee.net>
2015-04-12 21:19:33 -0600

    Update before removing from quahog

--- apps/osc_server/lib/osc_server.ex
+++ apps/osc_server/lib/osc_server.ex
@@ -1,2 +1,16 @@
 defmodule OSC.Server do
+  use Behaviour
+
+  def init() do
+  end
+
+  def start() do
+  end
+
+  def shutdown() do
+  end
+
+  defcallback receive() :: none
+
+  defcallback setup() :: none
 end
